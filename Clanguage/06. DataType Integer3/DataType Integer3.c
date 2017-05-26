b70c6037cc97a0d603dc799bf1d412db8ed4e442
동명 안 <gatlingfever34@gmail.com>
2017-05-26 18:57:36 +0900

    06 ing

--- Clanguage/06. DataType Integer3/DataType Integer3.c
+++ Clanguage/06. DataType Integer3/DataType Integer3.c
@@ -4,28 +4,16 @@
 
 main()
 {
-	signed char cChar = 0xff;
-	short nShort = 0xffff;
-	short nShort2 = 0xffff;
-	short nShort3 = 0xffff;
-	short nShort4 = 0xffff;
+	char cChar;
+	short nShort;
+	int nInteger;
+	long nLong;
 
-	int nInt = 0xffffffff;
-	long nLong = 0xffffffff;
+	printf("%p \n", &cChar);
+	printf("%p \n", &nShort);
+	printf("%p \n", &nInteger);
+	printf("%p \n", &nLong);
 
-	printf("%d \n", &cChar);
-	printf("%d \n", &nShort);
-	printf("%d \n", &nShort2);
-	printf("%d \n", &nShort3);
-	printf("%d \n", &nShort4);
-
-
-	printf("%d \n", &nInt);
-	printf("%d \n", &nLong);
-
-
-
-
-
+	
 	system("pause"); 
 }
